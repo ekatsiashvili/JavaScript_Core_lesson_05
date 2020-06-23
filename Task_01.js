1bffefebd05af749b7f417bc0f0bc2323d08fb56
Elena <katsiashvili.elena@gmail.com>
2020-06-23 11:42:27 +0300

    Add lesson_05_JS

---
+++ Task_01.js
@@ -0,0 +1,43 @@
+'use strict'
+
+function makeBuffer()
+{
+	let string = ''
+	
+	return {
+		getNext : function() {
+			string++
+		},
+		
+		add : function(value) {
+			string = string + '' + value
+		},
+		
+		print : function(){
+			return string
+		},
+		
+		clear : function(){
+			string = ''
+		}
+	}
+}
+
+
+let buffer = makeBuffer();
+
+buffer.add( 'JavaScript ');
+buffer.add( 'learn ');
+buffer.add( 'need to! ');
+console.log(buffer.print());
+
+buffer.add(0);
+buffer.add(0);
+buffer.add(0);
+console.log (buffer.print()); 
+
+buffer.add(" Test ");
+buffer.add("will not eat you!");
+console.log((buffer.print())); 
+buffer.clear ();
+console.log( (buffer.print())); 
